a8aec787c81615396809e8ce0caf331e18b15a0c
Sam Perumal <sam_lki@yahoo.com>
2019-05-28 22:07:40 +0200

    Simple format output

--- scores/index.js
+++ scores/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, req) {
 
     const response_body = {
         response_type: "ephemeral",
-        text: "Error running task: "
+        text: ""
     };
 
     if (req.body != "") {
@@ -15,37 +15,40 @@ module.exports = async function (context, req) {
         const parameters = parse(req.body);
 
         if (parameters.token != process.env["slack_token"]) {
-            response_body.text += ` ${parameters.token} != ${process.env["slack_token"]}`;
+            response_body.text += `Error: ${parameters.token} != ${process.env["slack_token"]}`;
         }
         if (parameters.team_id != process.env["team_id"]) {
-            response_body.text += ` ${parameters.token} != ${process.env["team_id"]}`;
+            response_body.text += `Error: ${parameters.token} != ${process.env["team_id"]}`;
         }
 
-        {
-            const re = /<(@\w+)([|]\w+)?/;
+        const re = /<(@\w+)([|]\w+)?/;
 
-            const match = re.exec(parameters.text);
+        const match = re.exec(parameters.text);
 
-            if (match != null && match.length > 1 && match[1] != null) {
-                const user = parameters.user_id;
-                const player = match[1];
-                response_body.text = `<@${user}> requested scores for <${player}>`;
+        if (match != null && match.length > 1 && match[1] != null) {
+            const user = parameters.user_id;
+            const player = match[1];
+            response_body.text = `<@${user}> requested scores for <${player}>\n`;
+        }
 
-                const fn = () => ({});
+        const fn = () => ({});
 
-                const game_data = await getBlob({
-                    azure_account: process.env["azure_account"],
-                    azure_account_key: process.env["azure_account_key"],
-                    containerName: process.env["azure_container"], 
-                    blobName: parameters.channel_id, 
-                    fn: fn,                    
-                });
+        const game_data_blob = await getBlob({
+            azure_account: process.env["azure_account"],
+            azure_account_key: process.env["azure_account_key"],
+            containerName: process.env["azure_container"],
+            blobName: parameters.channel_id,
+            fn: fn,
+        });
 
-                response_body.text += "\n" + JSON.stringify(game_data);
-            }
-            else response_body.text += ` ${parameters.text}`;
+        const game_data = JSON.parse(game_data_blob);
+
+        for (const player_id in game_data) {
+            const player = game_data[player_id];
+            response_body.text += `<@${player_id}>: ${player.money}\n`
         }
     }
+    else response_body.text += ` ${parameters.text}`;
 
     context.res = {
         headers: {
@@ -53,4 +56,4 @@ module.exports = async function (context, req) {
         },
         body: response_body
     };
-};
+}
